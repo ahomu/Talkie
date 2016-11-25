70c9a16829f8f7cc066d8a7f2ae46d356cd49265
Ayumu Sato <mail@ayumusato.com>
2016-11-25 17:58:37 +0900

    Fix up test

--- test/pointer.js
+++ test/pointer.js
@@ -8,24 +8,24 @@ describe('pointer', function() {
 
   it('coordBus', function() {
     let el = document.createElement('div');
-    let {coord} = Pointer(el);
+    let {ptCoord} = Pointer(el);
 
-    coord.next({clientX : 123, clientY : 456});
+    ptCoord.next({clientX : 123, clientY : 456});
     assert(el.style.left === '123px');
     assert(el.style.top === '456px');
 
-    coord.next({clientX : 789, clientY : 123});
+    ptCoord.next({clientX : 789, clientY : 123});
     assert(el.style.left === '789px');
     assert(el.style.top === '123px');
   });
 
   it('toggleBus', function() {
     let el = document.createElement('div');
-    let {toggle} = Pointer(el);
+    let {ptToggle} = Pointer(el);
 
-    toggle.next();
-    assert(el.style.visibility === 'visible');
-    toggle.next();
-    assert(el.style.visibility === 'hidden');
+    ptToggle.next();
+    assert(el.getAttribute('aria-hidden') === 'false');
+    ptToggle.next();
+    assert(el.getAttribute('aria-hidden') === 'true');
   });
 });
